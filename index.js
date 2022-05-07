53af3f249dc12fc414d140f53b9cd89b6a90a3ef
al-anas <alanas7171@gmail.com>
2022-05-08 01:37:23 +0600

    delete api added

--- index.js
+++ index.js
@@ -47,6 +47,14 @@ async function run(){
         res.send(result);
      })
 
+     //delete api
+     app.delete('/fruits/:id',async(req,res)=>{
+         const id = req.params.id;
+         const query = {_id:ObjectId(id)};
+         const result = await fruitsCollection.deleteOne(query);
+         res.send(result);
+     })
+
    }
    finally{
 
