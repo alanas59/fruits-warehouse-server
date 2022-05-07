39c02df6c8723798721da8d3554b7ddd9dfe8eb8
al-anas <alanas7171@gmail.com>
2022-05-07 23:56:53 +0600

    another fruits api included with id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //MiddleWare
@@ -18,11 +18,21 @@ async function run(){
    try{
      await client.connect();
      const fruitsCollection = client.db("fruitsInventory").collection("fruits");
+     //get all
      app.get('/fruits',async(req,res)=>{
         const query = {};
         const cursor = fruitsCollection.find(query);
         const fruits = await cursor.toArray();
         res.send(fruits);
+     })
+     //get single
+     app.get('/fruits/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id:ObjectId(id)};
+        const fruit = await fruitsCollection.findOne(query);
+        console.log(fruit);
+        res.send(fruit);
+
      })
 
    }
