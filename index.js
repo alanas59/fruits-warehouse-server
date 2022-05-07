fc480d8e749b18c323265d991c63cce3f0842632
al-anas <alanas7171@gmail.com>
2022-05-07 21:46:48 +0600

    mongodb installed and fruits api created

--- index.js
+++ index.js
@@ -2,11 +2,37 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //MiddleWare
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.64dyk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+   try{
+     await client.connect();
+     const fruitsCollection = client.db("fruitsInventory").collection("fruits");
+     app.get('/fruits',async(req,res)=>{
+        const query = {};
+        const cursor = fruitsCollection.find(query);
+        const fruits = await cursor.toArray();
+        res.send(fruits);
+     })
+
+   }
+   finally{
+
+   }
+}
+
+run().catch(console.dir);
+
 
 app.get('/',(req,res)=>{
     res.send('Hello World');
